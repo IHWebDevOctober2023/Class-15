07c15036246793619baca0a616a78312111cda57
marshall-bits <kz_@hotmail.es>
2023-11-08 14:13:01 +0100

    add solution

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Character = require('../models/character.model');
+const Episode = require('../models/episode.model');
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  Promise.all([Character.find(), Episode.find()])
+    .then(([characters, episodes]) => {
+      res.render("home", { characters, episodes });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
-router.get("/home", (req, res, next) => {
-  res.render("home");
+router.post("/character/add-to-episode", (req, res) => {
+  const { characterId, episodeId } = req.body;
+
+  Promise.all([
+    Character.findByIdAndUpdate(characterId, { $push: { episodes: episodeId } }),
+    Episode.findByIdAndUpdate(episodeId, { $push: { characters: characterId } })
+  ])
+    .then(() => {
+      res.render("success");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
 });
 
 module.exports = router;
